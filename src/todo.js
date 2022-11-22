a39450d949e7dfc73bb13b90682534200d49082d
onifernando1 <onifernando1@gmail.com>
2022-11-22 22:32:26 +0000

    new to do function

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,19 @@
 const toDo = function (title, description, dueDate, priority) {
-  return { title, description, dueDate, priority };
+
+    toDoStatus = incomplete
+
+    create(title, description, dueDate, priority){
+        newToDo = toDo(title, description, dueDate, priority)
+    }
+
+    destroy(){}
+
+    change(){}
+
+
+
+
+
+
+  return { title, description, dueDate, priority, toDoStatus };
 };
