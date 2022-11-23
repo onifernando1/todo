ca1759030297dd7f533b6da3f2647f83aa8438f2
onifernando1 <onifernando1@gmail.com>
2022-11-23 17:00:09 +0000

    create project manager factory

---
+++ src/projectManager.js
@@ -0,0 +1,11 @@
+const ProjectManager = function () {
+  const projectArray = [];
+
+  const showAllProjects = function () {
+    projectArray.forEach((project) => {
+      project.title;
+    });
+  };
+};
+
+export { ProjectManager };
